499aab8e5f5ae627015ebbb09effcc925278faea
alice <bow1226@gmail.com>
2022-03-08 17:57:43 +0800

    feat: Distance from circles to obstacles/actor

--- project/starter_files/cost_functions.cpp
+++ project/starter_files/cost_functions.cpp
@@ -67,10 +67,11 @@ double collision_circles_cost_spiral(const std::vector<PathPoint>& spiral,
           auto actor_center_y =
               obst.location.y + CIRCLE_OFFSETS[c2] * std::sin(actor_yaw);
 
-          // TODO-Distance from circles to obstacles/actor: How do you calculate
+          // Distance from circles to obstacles/actor: How do you calculate
           // the distance between the center of each circle and the
           // obstacle/actor
-          double dist = 0;  // <- Update
+          double dist = std::sqrt(std::pow(actor_center_x - circle_center_x, 2) + 
+                                 std::pow(actor_center_y - circle_center_y, 2));
 
           collision = (dist < (CIRCLE_RADII[c] + CIRCLE_RADII[c2]));
         }

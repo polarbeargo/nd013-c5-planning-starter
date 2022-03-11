c7365ba119cdc1bfca882595c4888a6d97d4921c
alice <bow1226@gmail.com>
2022-03-11 12:08:52 +0800

    fix: Simplify formula

--- project/starter_files/velocity_profile_generator.cpp
+++ project/starter_files/velocity_profile_generator.cpp
@@ -362,7 +362,7 @@ double VelocityProfileGenerator::calc_distance(const double& v_i,
     // v_i (initial velocity) to v_f (final velocity) at a constant
     // acceleration/deceleration "a". HINT look at the description of this
     // function. Make sure you handle div by 0
-    d = (v_f * v_f - v_i * v_i) / (2 * a);
+    d = (v_f^2 - v_i^2) / (2 * a);
   }
   return d;
 }
